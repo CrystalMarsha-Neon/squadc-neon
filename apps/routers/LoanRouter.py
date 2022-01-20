550fb88e6acb350f626ca10c7b566ef1e42bbc17
rezahabibineon <helloportfolio96@gmail.com>
2022-01-20 13:50:34 +0700

    add crud on loan

--- apps/routers/LoanRouter.py
+++ apps/routers/LoanRouter.py
@@ -8,6 +8,22 @@ example_input_cifno = json.dumps({
     "cif": "1",
 }, indent=2)
 
+example_input_ = json.dumps({
+    "loan_type": "1/2/3",
+    "loan_status": "1/2",
+    "loan_amount": "11,000,000",
+    "loan_tenure": "24",
+    "interest": "12",
+    "fname": "reza",
+    "lname": "habibi",
+    "dob": "mm/dd/yyyy",
+    "gender": "Female/Male",
+    "marital_status": "Single/Married",
+    "income": "22,000,000",
+    "phone": "333-4444-22",
+    "email": "r.habibi@ymail.com",
+}, indent=2)
+
 @router.post("/get_loan_by_cif")
 async def get_loan_by_cif(response: Response, input_data=Body(..., example=example_input_cifno)):
     result = loan.get_loan_by_cif(input_data=input_data)
@@ -20,3 +36,26 @@ async def get_loan_by_cif_debug(response: Response, input_data=Body(..., example
     response.status_code = result.status
     return result
 
+@router.get("/loan/{loanid}")
+async def get_loan(response: Response, loanid: int):
+    result = loan.get_loan(loanid=loanid)
+    response.status_code = result.status
+    return result
+
+@router.post("/loan")
+async def create_loan(response: Response, input_data=Body(..., example=example_input_)):
+    result = loan.create_loan(input_data=input_data)
+    response.status_code = result.status
+    return result
+
+@router.put("/loan/{loanid}")
+async def update_loan(response: Response, loanid: int, input_data=Body(..., example=example_input_)):
+    result = loan.update_loan(loanid=loanid, input_data=input_data)
+    response.status_code = result.status
+    return result
+
+@router.delete("/loan/{loanid}")
+async def delete_loan(response: Response, loanid: int):
+    result = loan.delete_loan(loanid=loanid)
+    response.status_code = result.status
+    return result
--- apps/schemas/SchemaCIF.py
+++ apps/schemas/SchemaCIF.py
@@ -4,23 +4,42 @@ from typing import Optional, List
 
 
 class RequestCIF(BaseModel):
-    cif: str = None
-
-class CIF(BaseModel):
-    loanid: str = None
     loan_type: int = None
     loan_status: int = None
-    loan_amount: int = None
+    loan_amount: str = None
     loan_tenure: int = None
     interest: int = None
-    cif: str  = None
+    cif: int = None
     idno: str = None
     fname: str = None
     lname: str = None
     dob: date = None
     gender: str = None
     marital_status: str = None
-    income: int = None
+    income: str = None
+    phone: str = None
+    email: str = None
+    isphoneverified: int = None
+    isemailverified: int = None
+    createdate: date = None
+    updatedate: date = None
+    source: str = None
+
+class CIF(BaseModel):
+    loanid: int = None
+    loan_type: int = None
+    loan_status: int = None
+    loan_amount: str = None
+    loan_tenure: int = None
+    interest: int = None
+    cif: int = None
+    idno: str = None
+    fname: str = None
+    lname: str = None
+    dob: date = None
+    gender: str = None
+    marital_status: str = None
+    income: str = None
     phone: str = None
     email: str = None
     isphoneverified: int = None
